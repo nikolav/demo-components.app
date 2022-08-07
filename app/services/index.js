9f7b6293eb4f75dda34b14096e0ade4532a444a1
nikolav <admin@nikolav.rs>
2022-08-07 04:01:05 +0100

    list 3rd.party services @app/store

---
+++ app/services/index.js
@@ -0,0 +1,5 @@
+import feathers from "./feathers";
+import firebase from "./firebase";
+import { supabase } from "./supabase";
+
+export { firebase, feathers, supabase };
--- src/data/index.js
+++
@@ -1,4 +0,0 @@
-import DonutChart from "./DonutChart";
-import piechart from "./piechart";
-
-export { DonutChart, piechart };
